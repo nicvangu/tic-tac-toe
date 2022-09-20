13ccc8e7b6b44f86d5671691d053065fc3d2bb83
nvangucht <nvangucht@outlook.com>
2022-09-20 12:13:51 -0400

    Configured project to use enzyme and created first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+
+  it('should be defined', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.length).toBe(1);
+  })
+
+})
