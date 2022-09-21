72a06f60055308406c6d21b8485f1e0aec72b72f
nvangucht <nvangucht@outlook.com>
2022-09-21 01:20:06 -0400

    Fixing typo

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -2,8 +2,9 @@ import './Tile.css';
 
 function Tile(props) {
     const { onClick, index, value } = props;
-    const ocuppiedClass = value != '' ? 'tile-occupied': '';
-    return <div id={`tile-${index}`} className={`tile ${ocuppiedClass}`} onClick={onClick}>{value}</div>
+    const occupiedClass = value != '' ? 'tile-occupied': '';
+    
+    return <div id={`tile-${index}`} className={`tile ${occupiedClass}`} onClick={onClick}>{value}</div>
 }
 
 export default Tile;
